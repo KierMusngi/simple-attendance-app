9bd94e790b5f07d08f70c5faca710a7fb1cec913
_sudothis <musngi.kier@gmail.com>
2022-04-09 21:40:25 +0800

    added axios for api requests

--- src/views/employees/index.js
+++ src/views/employees/index.js
@@ -1,15 +1,32 @@
+import { useEffect, useState } from 'react';
 import MainCard from 'ui-component/cards/MainCard';
 import DataTable from '../../ui-component/dataTable';
 import { Button } from '@mui/material';
+import axios from 'axios';
 
-const EmployeesPage = () => (
-    <MainCard title="Employees">
-        <DataTable />
-        <br />
-        <Button variant="contained" color="primary">
-            Add new employee
-        </Button>
-    </MainCard>
-);
+const EmployeesPage = () => {
+    const [employees, setEmployees] = useState();
+
+    const getEmployees = async () => {
+        await axios.get(`http://localhost:8080/employees`).then((res) => {
+            const employees = res.data;
+            setEmployees({ employees });
+        });
+    };
+
+    useEffect(() => {
+        getEmployees();
+    }, employees);
+
+    return (
+        <MainCard title="Employees">
+            <DataTable />
+            <br />
+            <Button variant="contained" color="primary">
+                Add new employee
+            </Button>
+        </MainCard>
+    );
+};
 
 export default EmployeesPage;
