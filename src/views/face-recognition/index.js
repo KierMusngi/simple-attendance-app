b3b59c5096d7e0e3085b6ca7ffaf77a3faeafff1
_sudothis <musngi.kier@gmail.com>
2022-05-28 11:00:38 +0800

    Added face detection

--- src/views/face-recognition/index.js
+++ src/views/face-recognition/index.js
@@ -1,14 +1,73 @@
 import { Grid, Typography, Card, CardMedia } from '@mui/material';
+import { useRef, useState, useCallback, useEffect } from 'react';
+import * as faceapi from 'face-api.js';
 import MainCard from 'ui-component/cards/MainCard';
 import Webcam from 'react-webcam';
 import { IconFaceId } from '@tabler/icons';
 
+faceapi.env.monkeyPatch({
+    Canvas: HTMLCanvasElement,
+    Image: HTMLImageElement,
+    ImageData: ImageData,
+    Video: HTMLVideoElement,
+    createCanvasElement: () => document.createElement('canvas'),
+    createImageElement: () => document.createElement('img')
+});
+
 const FaceRecognitionPage = () => {
+    const webcamRef = useRef(null);
+    const [imgSrc, setImgSrc] = useState(null);
+    const [isModelLoaded, setIsModelLoaded] = useState(false);
+
     const videoConstraints = {
         width: 600,
         height: 600,
         facingMode: 'user'
     };
+
+    const detectFace = () => {
+        if (!isModelLoaded) console.log('Models are not loaded');
+
+        setInterval(async () => {
+            const detections = await faceapi.detectSingleFace(webcamRef.current).withFaceLandmarks();
+            if (detections != null) console.log(detections);
+        }, 1000);
+    };
+
+    const startVideo = async () => {
+        const opt = { audio: false, video: true };
+        await navigator.mediaDevices
+            .getUserMedia(opt)
+            .then((stream) => {
+                webcamRef.current.srcObject = stream;
+            })
+            .catch((err) => console.log(err));
+    };
+
+    const loadModels = async () => {
+        Promise.all([
+            await faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+            await faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+            await faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+            await faceapi.nets.faceRecognitionNet.loadFromUri('/models')
+        ])
+            .then(() => {
+                setIsModelLoaded(true);
+                startVideo();
+                detectFace();
+            })
+            .catch((err) => console.log(err));
+    };
+
+    const capture = useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        setImgSrc(imageSrc);
+        console.log(imageSrc);
+    }, [webcamRef, setImgSrc]);
+
+    useEffect(() => {
+        loadModels();
+    }, []);
 
     return (
         <MainCard style={{ backgroundColor: '#EDE7F6' }}>
@@ -20,13 +79,19 @@ const FaceRecognitionPage = () => {
                 <Grid item>
                     <Card style={{ height: videoConstraints.height, width: videoConstraints.width }}>
                         <CardMedia>
-                            <Webcam
-                                audio={false}
-                                height={videoConstraints.height}
-                                screenshotFormat="image/jpeg"
-                                width={videoConstraints.width}
-                                videoConstraints={videoConstraints}
-                            />
+                            {!isModelLoaded && <p>Loading ...</p>}
+                            {isModelLoaded && (
+                                <Webcam
+                                    ref={webcamRef}
+                                    audio={false}
+                                    height={videoConstraints.height}
+                                    screenshotFormat="image/jpeg"
+                                    width={videoConstraints.width}
+                                    videoConstraints={videoConstraints}
+                                    onPlay={startVideo}
+                                />
+                            )}
+                            {/* <video ref={webcamRef} id="video" width="600" height="600" autoPlay muted /> */}
                         </CardMedia>
                     </Card>
                 </Grid>
@@ -34,6 +99,8 @@ const FaceRecognitionPage = () => {
                 <br />
                 <br />
                 <br />
+                <button onClick={capture}>Capture photo</button>
+                {imgSrc && <img src={imgSrc} alt="altimg" />}
                 <Grid item>
                     <IconFaceId color="#5E4BC3" size="100px" stroke="1px" />
                 </Grid>
