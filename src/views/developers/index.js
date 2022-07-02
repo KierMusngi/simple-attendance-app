7444a2d46e833ff7a8aed73f56572779a9f3e7ca
_sudothis <musngi.kier@gmail.com>
2022-07-02 14:37:02 +0800

    added developers tab

--- src/views/developers/index.js
+++ src/views/developers/index.js
@@ -1,7 +1,12 @@
 import MainCard from 'ui-component/cards/MainCard';
+import developers from 'assets/images/developers.JPG';
 
 const DevelopersPage = () => {
-    return <MainCard />;
+    return (
+        <MainCard>
+            <img src={developers} alt={'developers'} width={'100%'} />
+        </MainCard>
+    );
 };
 
 export default DevelopersPage;
